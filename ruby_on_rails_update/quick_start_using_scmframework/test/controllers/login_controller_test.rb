477994c5a152b3ae7f3dcb7b992ebb9309048a87
haymar1 <hm221473@gmail.com>
2022-01-25 09:59:20 +0630

    [feature/quickstartUsingSCMFramework]fix-review

---
+++ ruby_on_rails_update/quick_start_using_scmframework/test/controllers/login_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class LoginControllerTest < ActionDispatch::IntegrationTest
+  test "should get login" do
+    get login_login_url
+    assert_response :success
+  end
+
+  test "should get logout" do
+    get login_logout_url
+    assert_response :success
+  end
+end
